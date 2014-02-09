a15965306affa0dae9d2aeb516af894c30548b7c
Ozan Turgut <oturgut@turn.com>
2014-02-08 16:51:27 -0800

    Added async option to prevent test start before server start
    Signed-off-by: Ozan Turgut <oturgut@turn.com>

--- tasks/server.js
+++ tasks/server.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 module.exports = function(grunt){
 	grunt.registerMultiTask('server', 'Test server', function(){
+		var done = this.async();
 		var data = this.data;
 		var app = express();
 		app.use('/', express.static(data.root));
-		app.listen(data.port, data.hostname);
-	});
+		app.listen(data.port, data.hostname, done)
+;	});
 };
